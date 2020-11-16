46e88feff6c2f6b3e71f07ce7c7a3ae2fbcd5e92
Soumalya Mondal <58221994+Soumalya-Mondal@users.noreply.github.com>
2020-11-16 18:10:38 +0530

    Add files via upload

---
+++ C-D-image_train.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import os
+import random
+import pickle
+dir_dataset=r'C:\Users\Soumalya\Desktop\cat_vs_dog\dataset'
+CATEGORIES=['dog','cat']
+
+
+img_size=100
+
+data=[]
+
+
+for catagory in CATEGORIES:
+	folder=os.path.join(dir_dataset,catagory)
+	label=CATEGORIES.index(catagory)
+	for img in os.listdir(folder):
+		img_path=os.path.join(folder,img)
+		img_arr=cv2.imread(img_path)
+		img_arr=cv2.resize(img_arr,(img_size,img_size))
+		data.append([img_arr,label])
+random.shuffle(data)
+X=[]
+y=[]
+for features,label in data:
+	X.append(features)
+	y.append(label)
+X=np.array(X)
+y=np.array(y)
+pickle.dump(X, open('X.pkl', 'wb'))
+pickle.dump(y, open('y.pkl', 'wb'))
